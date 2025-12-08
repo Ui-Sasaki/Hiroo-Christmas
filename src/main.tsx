15c591333cfe267e7ec0c77f681473645b51e68d
MasaruGod <final-macbook@MasaruGodnoMacBook-Air.local>
2025-12-08 21:11:32 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home.tsx';
+import Home from './App.tsx';
 import Sales from './pages/sales/Sales';
 import './index.css';
 
